8c70927ee4bc734901db19ac16ab6fe32d965430
Richard Neher <richard@ag-neher-xps13.(none)>
2015-05-01 10:36:56 +0200

    added entropy quantile function -- stub

--- hivevo/HIVreference.py
+++ hivevo/HIVreference.py
@@ -20,11 +20,17 @@ class HIVreference(object):
 	def map_to_sequence_collection():
 		pass
 
+	@property	
 	def entropy(self):
 		pass
 
 	def consensus(self):
 		pass
 		
-
+	def get_entropy_quantiles(self, q):
+		from scipy.stats import score_at_percentile
+		thresholds = [score_at_percentile(self.entropy, 100.0*i/q) for i in range(q+1)]
+		return {i: {'range':(thresholds[i],thresholds[i+1], 
+					'ind':np.where((self.entropy>=thresholds[i])*(self.entropy<thresholds[i+1]))[0]}
+			   for i in range(q)}
 		
