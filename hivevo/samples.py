9b00d48656339681612dd81cfc51d1f03d6048c1
Richard <richard.neher@tuebingen.mpg.de>
2015-05-18 19:41:28 +0200

    commented sample class

--- hivevo/samples.py
+++ hivevo/samples.py
@@ -16,7 +16,9 @@ from itertools import izip
 all_fragments = ['F'+str(i) for i in range(1,7)]
 # Classes
 class Sample(pd.Series):
-    '''Patient sample'''
+    '''
+    Class providing access to sample specific quantities such as as depth, counts etc
+    '''
 
     def __init__(self, *args, **kwargs):
         '''Initialize a patient sample'''
@@ -28,7 +30,8 @@ class Sample(pd.Series):
         return Sample
 
     def get_n_templates_dilutions(self):
-        '''Get the time course of the number of templates to PCR, limiting depth'''
+        '''Get the number of templates to PCR calculated by interpolating the limiting dilution'''
+
         dilutions = np.array([1, 10,100,1000, 10000, 100000], dtype = int)
         successful = []
         dilstr = self.dilutions
@@ -39,16 +42,17 @@ class Sample(pd.Series):
             val = self['viral load']/60.0 
         else:
             try:
+                # extract the last positive dilution (dil_factor) and the number of successful dilutions 
                 dil_factor = float(dilstr.split()[0].split(':')[1])
                 n_positive = map(int, dilstr.split()[1][1:-1].split('/'))
                 for dil in dilutions:
-                    if dil<dil_factor:
+                    if dil<dil_factor: #assume all previous dilutions were successful
                         successful.append([2,2])
                     elif dil==dil_factor:
                         successful.append(n_positive)
                     else:
-                        successful.append([0,2])
-                successful = np.array(successful) # -> successful now contains [0] the number of successful dilutations in [1] trials
+                        successful.append([0,2])  # assume all subsequent dilutions failed
+                successful = np.array(successful) # -> successful now contains [:,0] the number of successful dilutions in [:, 1] trials
         
                 def prob(logcn, dil, suc):
                     # no amplifiable molecule: p=exp(-cn/dil*0.5) -> 2/2 (1-p)^2; 1/2: 2p(1-p); 0/2: p^2
@@ -57,6 +61,7 @@ class Sample(pd.Series):
                     p = np.exp(-np.exp(logcn)/dil*0.5)
                     return -np.sum( np.log((suc[:,0]==2)*(1-p)**2 + (suc[:,0]==1)*2*p*(1-p) + (suc[:,0]==0)*p**2 ))
         
+                # numerically optimize the log probability of observing the pattern of dilutions
                 from scipy.optimize import minimize
                 x_opt = minimize(prob, x0=2.0, args = (dilutions, successful), method='Powell')
                 val = np.exp(x_opt.x)
@@ -65,8 +70,11 @@ class Sample(pd.Series):
                 val = self['viral load']/60.0 
         return val
 
-    def _get_allele_counts(self, fragment, use_PCR1, VERBOSE):
-        '''Get allele counts for a single patient sample'''
+    def _get_fragment_counts(self, fragment, use_PCR1, VERBOSE):
+        '''
+        Get allele counts for a specific fragment of a specified PCR
+        this method is not meant for general use
+        '''
         if VERBOSE >= 1:
             print 'Getting allele counts:', self.patient, self.name, fragment
     
@@ -89,10 +97,19 @@ class Sample(pd.Series):
         return ac
 
     def get_allele_counts(self, coordinates, add = True, cov_min = 100, use_PCR1=1, VERBOSE=0, **kwargs):
+        '''
+        get counts at positions specified by coordinates
+        parameters:
+        coordinates  -- a dictionary with  fields 'F1': (array(pos in region of interest), array(pos on fragment))
+                        in addition, a field 'length': total length of the region of interest is required
+        add          -- if True, add counts when fragments overlap (default), other wise take max
+        use_PCR1     -- 1 == use PCR1 is available (default), 2 == force PCR1 or fail
+        cov_min      -- mask values where the final coverage is below that threshold
+        '''
         ac = np.ma.zeros((6, coordinates['length']), dtype = int)
         for fragment in all_fragments:
             if fragment in coordinates:
-                tmp_ac = self._get_allele_counts(fragment, use_PCR1, VERBOSE)
+                tmp_ac = self._get_fragment_counts(fragment, use_PCR1, VERBOSE)
                 if tmp_ac is not None:
                     if add:
                         ac[:,coordinates[fragment][0]]+=tmp_ac[:,coordinates[fragment][1]]
@@ -105,6 +122,9 @@ class Sample(pd.Series):
         return ac
 
     def get_allele_frequencies(self, coordinates, add=True, cov_min = 100, use_PCR1=1, VERBOSE=0, **kwargs):
+        '''
+        get counts at positions specified by coordinates -- all options as get_allele_counts
+        '''
         ac = self.get_allele_counts(coordinates, add=add, cov_min=cov_min, use_PCR1=use_PCR1, VERBOSE=VERBOSE, **kwargs)
         cov = ac.sum(axis=0)
         af = np.ma.masked_array(ac, dtype=float)
@@ -117,8 +137,13 @@ class Sample(pd.Series):
         return ac.sum(axis=0)
 
     def get_cocounts(self, fragment, use_PCR1=True, compressed=True):
+        '''
+        get joint counts of nuc1 at pos1 and nuc2 at pos2 for a fragment
+        returns:
+        ac     --  array of dimension (6,6,L,L) where L is the length of the fragment
+        '''
         from hivwholeseq.patients.filenames import get_allele_cocounts_filename
-        try:
+        try: # TODO: compressed or not needs cleaning up
             fname = get_allele_cocounts_filename(self.patient, self.name, fragment, PCR=1 if use_PCR1 else 2, compressed=compressed)
             ac = np.load(fname)['cocounts']
         except:
@@ -128,22 +153,32 @@ class Sample(pd.Series):
         return ac
 
     def get_pair_frequencies(self, fragment, var_min=0, use_PCR1=True, compressed=True):
+        '''
+        return the fraction of observations of nuc1, nuc2 and pos1, pos2 reduced to variable positions
+        parameters:
+        fragment -- the fragments to obtain the frequencies of
+        var_min  -- the minimal on site variability to be included in the matrix. 1-\sum_i x_i**2 >var_min
+        returns:
+        pos      -- variable positions
+        af2p     -- pair frequencies at variable positions
+        cov      -- coverage at the pairs
+        af1p     -- single nucleotide frequencies
+        '''
         import gc
-        #ac = self._get_allele_counts(fragment, use_PCR1, 0)
-        #af1p = np.array(ac, dtype=float)/(1e-10+ac.sum(axis=0))
-        #af1p.mask += np.repeat([ac.sum(axis=0)<cov_min], af1p.shape[0], axis=0)
         try:
             acc = self.get_cocounts(fragment, use_PCR1=use_PCR1, compressed=compressed)
         except:
             return_args = (None, None, None, None)
         else:
+            # calculate single nucleotide frequencies and determine variable sites
             af1p = np.array(acc[:,:,np.arange(acc.shape[2]), np.arange(acc.shape[3])].sum(axis=1),dtype=float)
             af1p = af1p/(1e-10+af1p.sum(axis=0))
-            variable_sites = np.sum(af1p**2, axis=0)<1.-var_min
-            if variable_sites.sum():
+            variable_sites = np.sum(af1p, axis=0)-np.sum(af1p**2, axis=0)>var_min
+            if variable_sites.sum()>1: # check whether there are at least 2 variable sites
                 reduced_af1p = af1p[:,variable_sites]
                 positions = np.where(variable_sites)[0]
                 n_variable_sites = reduced_af1p.shape[-1]
+                # make a reduced matric
                 reduced_af2p = np.zeros((acc.shape[0], acc.shape[1], n_variable_sites, n_variable_sites), dtype = float)
                 for di,dsite in enumerate(positions):
                     reduced_af2p[:,:,di,:] = acc[:,:,dsite,variable_sites]
@@ -159,6 +194,15 @@ class Sample(pd.Series):
         return return_args
 
     def fragment_depth(self, coordinates, var_min = 0.03, cov_min = 100, min_points = 5, pseudo_counts = 3):
+        '''
+        returns estimates of the fragment specific depth estimated from overlap frequencies
+        parameters:
+        coordinates   --  coordinates of all fragments as obtained for allele counts of the entire genome
+        var_min       --  minimal variance to include into the estimation
+        cov_min       --  minimal coverage to include into the estimation
+        min_points    --  the minimal number of valid frequencies required to attempt estimation
+        pseudo_counts --  number of additional pseudo_counts to include, these are equal to the depth estimate from dilutions
+        '''
         overlaps = zip(all_fragments[:-1], all_fragments[1:])
         # extract variant frequencies in overlaps
         overlap_frequencies = []
@@ -205,6 +249,7 @@ class Sample(pd.Series):
 
         return neff_fragments
 
+    # TODO: the following doesn't work 
     def haplotypes(self, fragment, start, stop, VERBOSE=0, maxreads=-1, filters=None, PCR=1):
         from hivwholeseq.patients.get_local_haplotypes import get_local_haplotypes
         from hivwholeseq.patients.filenames import get_mapped_filtered_filename
