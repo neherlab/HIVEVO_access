0f9b1732aafb92df0284b9a794ac92fe8dcb8430
Richard <richard.neher@tuebingen.mpg.de>
2015-05-18 10:48:33 +0200

    replaced 1.0 by the sum over allele frequencies at a site

--- hivevo/af_tools.py
+++ hivevo/af_tools.py
@@ -9,10 +9,10 @@ import numpy as np
 from hivwholeseq.utils.sequence import alpha, alphaa
 
 def diversity(af):
-    return 1 - np.mean(np.sum(af**2, axis=0))
+    return np.mean(af.sum(axis=0)) - np.mean(np.sum(af**2, axis=0))
 
 def divergence(af, initial):
-    return 1 - np.mean(af[initial,np.arange(len(initial))])
+    return np.mean(af.sum(axis=0)) - np.mean(af[initial,np.arange(len(initial))])
 
 def majority_frequency(af):
     return np.max(af, axis=0)
